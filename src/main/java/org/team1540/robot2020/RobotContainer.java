02f6fcabc7c03ed5b513d0c97335c8e0d5a2e190
Liam Wang <5054950+wangl5@users.noreply.github.com>
2020-01-08 16:00:37 -0800

    Copy auto command from wpilib

--- src/main/java/org/team1540/robot2020/RobotContainer.java
+++ src/main/java/org/team1540/robot2020/RobotContainer.java
@@ -5,10 +5,21 @@ import static org.team1540.rooster.util.ChickenXboxController.XboxButton.Y;
 
 import edu.wpi.first.wpilibj.RobotState;
 import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.controller.RamseteController;
+import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveVoltageConstraint;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import java.util.List;
 import org.apache.log4j.Logger;
 import org.team1540.robot2020.commands.drivetrain.ArcadeDrive;
 import org.team1540.robot2020.shouldbeinrooster.InstCommand;
@@ -65,6 +76,63 @@ public class RobotContainer {
                 // disable brakes
                 logger.info("Mechanism brakes disabled");
             }, true), RobotState::isEnabled)));
+
+    }
+
+    public Command getAutoCommand() {
+
+        // Create a voltage constraint to ensure we don't accelerate too fast
+        var autoVoltageConstraint =
+            new DifferentialDriveVoltageConstraint(
+                new SimpleMotorFeedforward(driveTrain.ksVolts,
+                    driveTrain.kvVoltSecondsPerMeter,
+                    driveTrain.kaVoltSecondsSquaredPerMeter),
+                driveTrain.kDriveKinematics,
+                10);
+
+        // Create config for trajectory
+        TrajectoryConfig config =
+            new TrajectoryConfig(driveTrain.kMaxSpeedMetersPerSecond,
+                driveTrain.kMaxAccelerationMetersPerSecondSquared)
+                // Add kinematics to ensure max speed is actually obeyed
+                .setKinematics(driveTrain.kDriveKinematics)
+                // Apply the voltage constraint
+                .addConstraint(autoVoltageConstraint);
+
+        // An example trajectory to follow.  All units in meters.
+        Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
+            // Start at the origin facing the +X direction
+            new Pose2d(0, 0, new Rotation2d(0)),
+            // Pass through these two interior waypoints, making an 's' curve path
+            List.of(
+//                new Translation2d(1, 1),
+//                new Translation2d(2, -1)
+            ),
+            // End 3 meters straight ahead of where we started, facing forward
+            new Pose2d(3, 0, new Rotation2d(0)),
+            // Pass config
+            config
+        );
+
+        RamseteCommand ramseteCommand = new RamseteCommand(
+            exampleTrajectory,
+            driveTrain::getPose,
+            new RamseteController(driveTrain.kRamseteB, driveTrain.kRamseteZeta),
+            new SimpleMotorFeedforward(driveTrain.ksVolts,
+                driveTrain.kvVoltSecondsPerMeter,
+                driveTrain.kaVoltSecondsSquaredPerMeter),
+            driveTrain.kDriveKinematics,
+            driveTrain::getWheelSpeeds,
+            new PIDController(driveTrain.kPDriveVel, 0, 0),
+            new PIDController(driveTrain.kPDriveVel, 0, 0),
+            // RamseteCommand passes volts to the callback
+            driveTrain::tankDriveVolts,
+            driveTrain
+        );
+
+        // Run path following command, then stop at the end.
+//            ramseteCommand.andThen(() -> driveTrain.tankDriveVolts(0, 0)).schedule();
+        return ramseteCommand;
     }
 
     private void initDefaultCommands() {
