c981e3180545b0e0a2b29f2d33c8dd928d964b17
Nathan Merrill <mathiscool3000@gmail.com>
2020-01-14 15:48:59 +0000

    Add MoveBallsUp

--- src/main/java/org/team1540/robot2020/subsystems/Indexer.java
+++ src/main/java/org/team1540/robot2020/subsystems/Indexer.java
@@ -2,14 +2,27 @@ package org.team1540.robot2020.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Indexer extends SubsystemBase {
     private CANSparkMax cord = new CANSparkMax(0, CANSparkMaxLowLevel.MotorType.kBrushless);
+
+    private DigitalInput stagingSensor = new DigitalInput(0);
+    private DigitalInput topSensor = new DigitalInput(0);
+
     private int balls = 0;
 
     public void setSpeed(double speed) {
         cord.set(speed);
+    }
+
+    public Boolean getStagingSensor() {
+        return stagingSensor.get();
+    }
+
+    public Boolean getTopSensor() {
+        return topSensor.get();
     }
 
     public int getBalls() {
