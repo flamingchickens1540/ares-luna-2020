f88bae7ae989c91fa2793c4c894a1d87787f1145
Nathan Merrill <mathiscool3000@gmail.com>
2020-01-27 17:01:26 +0000

    Rename climber motor

--- src/main/java/org/team1540/robot2020/subsystems/Climber.java
+++ src/main/java/org/team1540/robot2020/subsystems/Climber.java
@@ -6,7 +6,7 @@ import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climber extends SubsystemBase {
-    private TalonFX climberA = new TalonFX(12);
+    private TalonFX climber = new TalonFX(12);
 
     private Servo ratchet = new Servo(0);
 
@@ -15,11 +15,11 @@ public class Climber extends SubsystemBase {
     }
 
     public void setPercent(double percent) {
-        climberA.set(ControlMode.PercentOutput, percent);
+        climber.set(ControlMode.PercentOutput, percent);
     }
 
     public double getPosition() {
-        return climberA.getSelectedSensorPosition();
+        return climber.getSelectedSensorPosition();
     }
 
     public void setRatchet(Boolean on) {
