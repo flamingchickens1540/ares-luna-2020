c3e7b2e0181d05f46024cec122b7abdc883536bc
Nathan Merrill <mathiscool3000@gmail.com>
2020-01-27 16:18:44 +0000

    Remove climberB

--- src/main/java/org/team1540/robot2020/subsystems/Climber.java
+++ src/main/java/org/team1540/robot2020/subsystems/Climber.java
@@ -6,11 +6,6 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climber extends SubsystemBase {
     private TalonFX climberA = new TalonFX(12);
-    private TalonFX climberB = new TalonFX(13);
-
-    public Climber() {
-        climberB.follow(climberA);
-    }
 
     public void setPercent(double percent) {
         climberA.set(ControlMode.PercentOutput, percent);
