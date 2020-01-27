c04f1cdacd2fade1015aca1cd603093799bd19e1
Nathan Merrill <mathiscool3000@gmail.com>
2020-01-27 15:49:43 +0000

    Update shooter CAN id

--- src/main/java/org/team1540/robot2020/subsystems/Shooter.java
+++ src/main/java/org/team1540/robot2020/subsystems/Shooter.java
@@ -5,8 +5,8 @@ import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
-    private TalonFX shooterA = new TalonFX(7);
-    private TalonFX shooterB = new TalonFX(8);
+    private TalonFX shooterA = new TalonFX(8);
+    private TalonFX shooterB = new TalonFX(9);
 
     public Shooter() {
         shooterB.follow(shooterA);
