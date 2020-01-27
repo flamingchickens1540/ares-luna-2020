0e00314503c760461db10d243ed29efdbb44d6e8
Nathan Merrill <mathiscool3000@gmail.com>
2020-01-27 16:50:08 +0000

    Convert climber pid to ProfiledPIDController

--- src/main/java/org/team1540/robot2020/commands/climber/MoveClimberToPosition.java
+++ src/main/java/org/team1540/robot2020/commands/climber/MoveClimberToPosition.java
@@ -1,19 +1,22 @@
 package org.team1540.robot2020.commands.climber;
 
 import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.controller.ProfiledPIDController;
+import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import org.team1540.robot2020.subsystems.Climber;
 
 public class MoveClimberToPosition extends CommandBase {
     private Climber climber;
-    private PIDController pidController = new PIDController(1, 0, 0);
+    private ProfiledPIDController pidController = new ProfiledPIDController(1, 0, 0,
+            new TrapezoidProfile.Constraints(10, 20));
     private double position;
 
     public MoveClimberToPosition(Climber climber, double position) {
         this.climber = climber;
         this.position = position;
         addRequirements(climber);
-        pidController.setSetpoint(position);
+        pidController.setGoal(position);
     }
 
     @Override
