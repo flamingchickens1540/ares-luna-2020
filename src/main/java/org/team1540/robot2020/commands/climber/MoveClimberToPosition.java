205d3a8bc0b07f587724af4f8bb3a57bbf9c6c85
Nathan Merrill <mathiscool3000@gmail.com>
2020-01-27 16:41:27 +0000

    Add ratchet control to Climber

--- src/main/java/org/team1540/robot2020/commands/climber/MoveClimberToPosition.java
+++ src/main/java/org/team1540/robot2020/commands/climber/MoveClimberToPosition.java
@@ -7,15 +7,30 @@ import org.team1540.robot2020.subsystems.Climber;
 public class MoveClimberToPosition extends CommandBase {
     private Climber climber;
     private PIDController pidController = new PIDController(1, 0, 0);
+    private double position;
 
     public MoveClimberToPosition(Climber climber, double position) {
         this.climber = climber;
+        this.position = position;
         addRequirements(climber);
         pidController.setSetpoint(position);
     }
 
     @Override
+    public void initialize() {
+//        20 is half the climber's height
+        if (position >= 20) {
+            climber.setRatchet(false);
+        }
+    }
+
+    @Override
     public void execute() {
         climber.setPercent(pidController.calculate(climber.getPosition()));
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        climber.setRatchet(true);
     }
 }
--- src/main/java/org/team1540/robot2020/subsystems/Climber.java
+++ src/main/java/org/team1540/robot2020/subsystems/Climber.java
@@ -2,10 +2,17 @@ package org.team1540.robot2020.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climber extends SubsystemBase {
     private TalonFX climberA = new TalonFX(12);
+
+    private Servo ratchet = new Servo(0);
+
+    public Climber() {
+        setRatchet(true);
+    }
 
     public void setPercent(double percent) {
         climberA.set(ControlMode.PercentOutput, percent);
@@ -13,5 +20,9 @@ public class Climber extends SubsystemBase {
 
     public double getPosition() {
         return climberA.getSelectedSensorPosition();
+    }
+
+    public void setRatchet(Boolean on) {
+        ratchet.set(on ? 1 : 0);
     }
 }
