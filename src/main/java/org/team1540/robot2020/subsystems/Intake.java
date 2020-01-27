f73abf052d3c4759658159508970d9b656fa4552
Nathan Merrill <mathiscool3000@gmail.com>
2020-01-27 15:42:27 +0000

    Set Intake motor CAN id

--- src/main/java/org/team1540/robot2020/subsystems/Intake.java
+++ src/main/java/org/team1540/robot2020/subsystems/Intake.java
@@ -5,7 +5,7 @@ import com.revrobotics.CANSparkMaxLowLevel;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
-    private CANSparkMax rollers = new CANSparkMax(0, CANSparkMaxLowLevel.MotorType.kBrushless);
+    private CANSparkMax rollers = new CANSparkMax(4, CANSparkMaxLowLevel.MotorType.kBrushless);
 
     public void setRollerSpeed(double speed) {
         rollers.set(speed);
