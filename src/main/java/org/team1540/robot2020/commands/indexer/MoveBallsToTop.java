54b6c592189685d1a12071c70c56d3249e6d6c92
Nathan Merrill <mathiscool3000@gmail.com>
2020-01-28 15:55:00 +0000

    Fix MoveBallsToTop indexer speed

--- src/main/java/org/team1540/robot2020/commands/indexer/MoveBallsToTop.java
+++ src/main/java/org/team1540/robot2020/commands/indexer/MoveBallsToTop.java
@@ -1,6 +1,5 @@
 package org.team1540.robot2020.commands.indexer;
 
-import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import org.team1540.robot2020.subsystems.Indexer;
 
@@ -13,7 +12,7 @@ public class MoveBallsToTop extends CommandBase {
 
     @Override
     public void initialize() {
-        indexer.setPercent(100);
+        indexer.setPercent(1);
     }
 
     @Override
