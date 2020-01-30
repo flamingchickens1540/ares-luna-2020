d47576870f9fe12179fc96c26b86eaf35a13e6e1
Nathan Merrill <mathiscool3000@gmail.com>
2020-01-30 16:21:38 +0000

    Remove unused imports

--- src/main/java/org/team1540/robot2020/commands/climber/MoveClimberToPosition.java
+++ src/main/java/org/team1540/robot2020/commands/climber/MoveClimberToPosition.java
@@ -1,7 +1,5 @@
 package org.team1540.robot2020.commands.climber;
 
-import edu.wpi.first.wpilibj.controller.ProfiledPIDController;
-import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj2.command.*;
 import org.team1540.robot2020.subsystems.Climber;
 import org.team1540.robot2020.utils.InstCommand;
