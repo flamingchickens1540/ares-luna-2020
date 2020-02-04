30883c3e5ef68a98814df0277977b5466657d574
Nathan Merrill <mathiscool3000@gmail.com>
2020-02-04 16:29:16 +0000

    Remove todo

--- src/main/java/org/team1540/robot2020/commands/indexer/BallQueueSequence.java
+++ src/main/java/org/team1540/robot2020/commands/indexer/BallQueueSequence.java
@@ -24,7 +24,6 @@ public class BallQueueSequence extends SequentialCommandGroup {
     @Override
     public void end(boolean interrupted) {
         if (!interrupted) {
-            // TODO test if this works with Phineas
             this.schedule();
         }
     }
