4ed55fef3bb793de188f8f70ac09b71c07a7af48
Nate Sales <nate@nate.to>
2020-02-03 18:31:16 -0800

    Add ZeroShooterHood

---
+++ src/main/java/org/team1540/robot2020/commands/shooter/ZeroShooterHood.java
@@ -0,0 +1,33 @@
+package org.team1540.robot2020.commands.shooter;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import org.team1540.robot2020.subsystems.Shooter;
+
+public class ZeroShooterHood extends CommandBase {
+    private Shooter shooter;
+    private boolean _isFinished = false;
+
+    private final int HOOD_SPEED_PERCENT = 0;
+
+    public ZeroShooterHood(Shooter shooter) {
+        this.shooter = shooter;
+    }
+
+    @Override
+    public void execute() {
+        if (!shooter.limitSwitchPressed()) {
+            shooter.setHoodPercent(HOOD_SPEED_PERCENT);
+        } else {
+            _isFinished = true;
+        }
+
+        SmartDashboard.putBoolean("shooter/limit_switch", shooter.limitSwitchPressed());
+        SmartDashboard.putNumber("shooter/hood_percent", shooter.getHoodPercent());
+    }
+
+    @Override
+    public boolean isFinished() {
+        return _isFinished;
+    }
+}
