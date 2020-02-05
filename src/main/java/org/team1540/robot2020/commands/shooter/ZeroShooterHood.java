95ace1a4a802717499b535410880f1f4279a5759
Nate Sales <nate@nate.to>
2020-02-05 15:58:25 -0800

    Fix unnecessary local

--- src/main/java/org/team1540/robot2020/commands/shooter/ZeroShooterHood.java
+++ src/main/java/org/team1540/robot2020/commands/shooter/ZeroShooterHood.java
@@ -1,13 +1,10 @@
 package org.team1540.robot2020.commands.shooter;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import org.team1540.robot2020.subsystems.Shooter;
 
 public class ZeroShooterHood extends CommandBase {
-    private final int HOOD_SPEED_PERCENT = 0;
     private Shooter shooter;
-    private boolean _isFinished = false;
 
     public ZeroShooterHood(Shooter shooter) {
         this.shooter = shooter;
@@ -15,21 +12,11 @@ public class ZeroShooterHood extends CommandBase {
 
     @Override
     public void execute() {
-        // TODO the execute method will stop when the command is over, so you don't need to check the limit switch here
-        if (!shooter.isLimitSwitchPressed()) {
-            shooter.setHoodPercent(HOOD_SPEED_PERCENT);
-        } else {
-            _isFinished = true;
-        }
-
-        // TODO these should be put in the shooter periodic
-        SmartDashboard.putBoolean("shooter/limit_switch", shooter.isLimitSwitchPressed());
-        SmartDashboard.putNumber("shooter/hood_percent", shooter.getHoodPercent());
+        shooter.setHoodPercent(25);
     }
 
     @Override
     public boolean isFinished() {
-        // TODO the isFinished method runs periodically so you can just return shooter.isLimitSwitchPressed()
-        return _isFinished;
+        return shooter.isLimitSwitchPressed();
     }
 }
