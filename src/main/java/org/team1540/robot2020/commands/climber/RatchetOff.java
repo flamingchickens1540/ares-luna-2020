8ac0ad098099cea47fb4c372d8e3ded1b3bd3b0e
Nathan Merrill <mathiscool3000@gmail.com>
2020-02-07 15:51:02 +0000

    Add RatchetOn and RatchetOff

---
+++ src/main/java/org/team1540/robot2020/commands/climber/RatchetOff.java
@@ -0,0 +1,17 @@
+package org.team1540.robot2020.commands.climber;
+
+import org.team1540.robot2020.subsystems.Climber;
+import org.team1540.robot2020.utils.InstCommand;
+
+public class RatchetOff extends InstCommand {
+    private Climber climber;
+
+    public RatchetOff(Climber climber) {
+        this.climber = climber;
+    }
+
+    @Override
+    public void initialize() {
+        climber.setRatchet(Climber.RatchetState.OFF);
+    }
+}
