c208fdc55967d331c900056e52c2c649ef2e21b6
Nate Sales <nate@nate.to>
2020-02-08 16:34:25 -0800

    Move hood commands to new subpackage

--- src/main/java/org/team1540/robot2020/commands/hood/ZeroHood.java
+++ src/main/java/org/team1540/robot2020/commands/hood/ZeroHood.java
@@ -1,19 +1,21 @@
-package org.team1540.robot2020.commands.shooter;
+package org.team1540.robot2020.commands.hood;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import org.team1540.robot2020.subsystems.Hood;
 
 public class ZeroHood extends CommandBase {
+    private final double speed;
     private Hood hood;
 
-    public ZeroHood(Hood hood) {
+    public ZeroHood(Hood hood, double speed) {
         this.hood = hood;
+        this.speed = speed;
         addRequirements(hood);
     }
 
     @Override
     public void initialize() {
-        hood.setPercent(25);
+        hood.setPercent(speed);
     }
 
     @Override
