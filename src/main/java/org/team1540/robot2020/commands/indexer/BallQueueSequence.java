2eba1ebee6059c251a56f63ae20a12d429bb431a
Liam Wang <5054950+wangl5@users.noreply.github.com>
2020-02-09 17:48:20 -0800

    Stop indexing if full

--- src/main/java/org/team1540/robot2020/commands/indexer/BallQueueSequence.java
+++ src/main/java/org/team1540/robot2020/commands/indexer/BallQueueSequence.java
@@ -1,5 +1,6 @@
 package org.team1540.robot2020.commands.indexer;
 
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -11,32 +12,57 @@ public class BallQueueSequence extends SequentialCommandGroup {
     public BallQueueSequence(Indexer indexer, Funnel funnel) {        // TODO: ATTN this doesn't work
         addRequirements(indexer, funnel);
         addCommands(
-                new IndexerStagedForAWhile(indexer, 0.5),
                 race(
-                        sequence(new FunctionalCommand(
-                                        () -> indexer.setPercent(0.3),
-                                        () -> {
-                                        },
-                                        (interrupted) -> indexer.bottomOfBottomBallMeters = indexer.getPositionMeters(),
-                                        () -> !indexer.getIndexerStagedSensor(),
-                                        indexer),
-                                new IndexerMoveToPosition(indexer, () -> indexer.bottomOfBottomBallMeters + 0.02, 0.3, 0.001),
-                                new InstantCommand(indexer::ballAdded)),
+                        sequence(
+                                new IndexerStagedForAWhile(indexer, 0.5),
+                                race(
+                                        sequence(new FunctionalCommand(
+                                                        () -> indexer.setPercent(0.3),
+                                                        () -> {
+                                                        },
+                                                        (interrupted) -> indexer.bottomOfBottomBallMeters = indexer.getPositionMeters(),
+                                                        () -> !indexer.getIndexerStagedSensor(),
+                                                        indexer),
+                                                new IndexerMoveToPosition(indexer, () -> indexer.bottomOfBottomBallMeters + 0.02, 0.3, 0.001),
+                                                new InstantCommand(indexer::ballAdded)),
+                                        new FunctionalCommand(
+                                                () -> funnel.setPercent(0.5, 0.1),
+                                                () -> {
+                                                },
+                                                (interrupted) -> funnel.stop(),
+                                                () -> false,
+                                                funnel
+                                        )
+                                )
+                        ),
                         new FunctionalCommand(
-                                () -> funnel.setPercent(0.5, 0.1),
                                 () -> {
                                 },
-                                (interrupted) -> funnel.stop(),
-                                () -> false,
-                                funnel
+                                () -> {
+                                },
+                                (interrupted) -> endFlag = !interrupted,
+                                indexer::getShooterStagedSensor
                         )
-                )
+                ),
+                new ConditionalCommand(
+                        new StageBallsForShooting(indexer),
+                        new InstantCommand(),
+                        () -> endFlag)
         );
+    }
+
+    private boolean endFlag;
+
+    @Override
+    public void initialize() {
+        endFlag = false;
+        super.initialize();
     }
 
     @Override
     public void end(boolean interrupted) {
-        if (!interrupted) {
+        super.end(interrupted);
+        if (!interrupted && !endFlag) {
             this.schedule();
         }
     }
