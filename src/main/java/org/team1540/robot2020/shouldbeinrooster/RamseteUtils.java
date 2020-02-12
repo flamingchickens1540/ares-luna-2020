5ad3003df159fc572158ba5c990d6c3e2c41984d
Nathan Merrill <mathiscool3000@gmail.com>
2020-02-12 16:24:27 +0000

    Add translateWaypoints

--- src/main/java/org/team1540/robot2020/shouldbeinrooster/RamseteUtils.java
+++ src/main/java/org/team1540/robot2020/shouldbeinrooster/RamseteUtils.java
@@ -2,6 +2,10 @@ package org.team1540.robot2020.shouldbeinrooster;
 
 import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Transform2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RamseteUtils {
     public static Pose2d translatePose(Pose2d current, Pose2d dest) {
@@ -10,5 +14,14 @@ public class RamseteUtils {
                 transform.getTranslation(),
                 transform.getRotation()
         );
+    }
+
+    public static List<Translation2d> translateWaypoints(Pose2d current, List<Translation2d> waypoints) {
+        Translation2d currentTranslation = current.getTranslation();
+        List<Translation2d> outputWaypoints = new ArrayList<>();
+        for (Translation2d waypoint : waypoints) {
+            outputWaypoints.add(waypoint.minus(currentTranslation));
+        }
+        return outputWaypoints;
     }
 }
