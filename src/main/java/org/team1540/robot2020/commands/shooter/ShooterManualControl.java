7ad4de14faefdc14f91ce6f8907e2943e4b75ea3
Nate Sales <nate@nate.to>
2020-02-14 18:07:37 -0800

    Fix typo

--- src/main/java/org/team1540/robot2020/commands/shooter/ShooterManualControl.java
+++ src/main/java/org/team1540/robot2020/commands/shooter/ShooterManualControl.java
@@ -4,17 +4,17 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import org.team1540.robot2020.utils.ChickenXboxController;
 
 public class ShooterManualControl extends CommandBase {
-    private Shooter intake;
+    private Shooter shooter;
     private ChickenXboxController.Axis joystickAxis;
 
     public ShooterManualControl(Shooter shooter, ChickenXboxController.Axis joystickAxis) {
-        this.intake = shooter;
+        this.shooter = shooter;
         this.joystickAxis = joystickAxis;
         addRequirements(shooter);
     }
 
     @Override
     public void execute() {
-        intake.setPercent(joystickAxis.value());
+        shooter.setPercent(joystickAxis.value());
     }
 }
