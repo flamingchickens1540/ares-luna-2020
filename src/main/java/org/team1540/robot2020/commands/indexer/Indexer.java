933a18648c4bbf5d6c5823e56c2b2f81464b7f6e
Zachary Robinson <robinsonz@catlin.edu>
2020-02-15 08:57:14 -0800

    Add stop() and commmandStop() to indexer

--- src/main/java/org/team1540/robot2020/commands/indexer/Indexer.java
+++ src/main/java/org/team1540/robot2020/commands/indexer/Indexer.java
@@ -9,6 +9,7 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.team1540.robot2020.utils.MotorConfigUtils;
@@ -84,6 +85,10 @@ public class Indexer extends SubsystemBase {
         return new StartEndCommand(() -> setPercent(percent), () -> setPercent(0));
     }
 
+    public Command commandStop() {
+        return new InstantCommand(this::stop, this);
+    }
+
     public void setPositionMeters(double positionMeters, double speed) {
         indexerMotor.set(ControlMode.Position, positionMeters * ticksPerMeter);
     }
@@ -122,5 +127,9 @@ public class Indexer extends SubsystemBase {
 
     public void ballAdded() {
         ballPositions.add(0, getPositionMeters());
+    }
+
+    public void stop() {
+        setPercent(0);
     }
 }
