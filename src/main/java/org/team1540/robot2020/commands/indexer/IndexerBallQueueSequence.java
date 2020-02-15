a89c3ce2124631528f653ac748dbce6f85fe2ab1
Zachary Robinson <robinsonz@catlin.edu>
2020-02-15 09:00:26 -0800

    Remove command decorators from IndexerBallQueueSequence
    liam y tho

--- src/main/java/org/team1540/robot2020/commands/indexer/IndexerBallQueueSequence.java
+++ src/main/java/org/team1540/robot2020/commands/indexer/IndexerBallQueueSequence.java
@@ -5,27 +5,36 @@ import org.team1540.robot2020.commands.funnel.Funnel;
 import org.team1540.robot2020.commands.funnel.FunnelRun;
 
 public class IndexerBallQueueSequence extends SequentialCommandGroup {
-    private boolean endFlag;
-
     public IndexerBallQueueSequence(Indexer indexer, Funnel funnel) {
         addRequirements(indexer, funnel);
         addCommands(
-                new ConditionalCommand(new PrintCommand("Indexer not running because top sensor is tripped"),
+                new ConditionalCommand( // only run if the top sensor isn't tripped
+                        new PrintCommand("Indexer not running because top sensor is tripped"),
                         race(
-                                new WaitUntilCommand(indexer::getShooterStagedSensor)
-                                        .andThen(new ScheduleCommand(
-                                                new IndexerBallsToTop(indexer, Indexer.secondIndexingSpeed)
-                                                        .andThen(new StartEndCommand(() -> indexer.setPercent(Indexer.secondIndexingSpeed), () -> indexer.setPercent(0)).withTimeout(0.1))
-                                        )),
-                                new InstantCommand(() -> indexer.setPercent(0))
-                                        .andThen(new WaitForDebouncedIndexerSensor(indexer, 0.25))
-                                        .andThen(new RunCommand(() -> indexer.setPercent(Indexer.firstIndexingSpeed))
-                                                .withInterrupt(() -> !indexer.getIndexerStagedSensor())
-                                                .andThen(() -> indexer.setPercent(0))
-                                                .andThen(indexer::ballAdded))
-                                        .andThen(new InstantCommand(() -> new IndexerBallQueueSequence(indexer, funnel).schedule())) // not a schedule command to avoid stack overflow
-                                        .raceWith(new FunnelRun(funnel))
-                        ), indexer::getShooterStagedSensor)
+                                sequence( // wait for balls to reach the top of the indexer, then stop grabbing new balls and line things up
+                                        new WaitUntilCommand(indexer::getShooterStagedSensor),
+                                        // schedulecommand so that it immediately interrupts this command group even if we're moving a ball in from the funnel
+                                        new ScheduleCommand(sequence(
+                                                new IndexerBallsToTop(indexer, Indexer.secondIndexingSpeed),
+                                                indexer.commandPercent(Indexer.secondIndexingSpeed).withTimeout(0.1)
+                                        ))
+                                ),
+                                race(
+                                        new FunnelRun(funnel),
+                                        sequence(
+                                                indexer.commandStop(),
+                                                new WaitForDebouncedIndexerSensor(indexer, 0.25),
+                                                indexer.commandPercent(Indexer.firstIndexingSpeed).withInterrupt(() -> !indexer.getIndexerStagedSensor()),
+                                                new InstantCommand(() -> {
+                                                    indexer.ballAdded();
+                                                    // not a schedulecommand to avoid stack overflow on init
+                                                    new IndexerBallQueueSequence(indexer, funnel).schedule();
+                                                })
+                                        )
+                                )
+                        ),
+                        indexer::getShooterStagedSensor
+                )
         );
     }
 }
