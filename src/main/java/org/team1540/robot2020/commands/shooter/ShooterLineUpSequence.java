6c520de848d1e122d25e54b38aae9d2f2ea61ff3
Nate Sales <nate@nate.to>
2020-02-14 21:31:40 -0800

    Fix lookup table

--- src/main/java/org/team1540/robot2020/commands/shooter/ShooterLineUpSequence.java
+++ src/main/java/org/team1540/robot2020/commands/shooter/ShooterLineUpSequence.java
@@ -35,36 +35,36 @@ public class ShooterLineUpSequence extends SequentialCommandGroup {
     }
 
     private double line(double x1, double y1, double x2, double y2, double x) {
-        return ((y2 - y1) / (x2 - x2)) * (x - x1) + y2;
+        return ((y2 - y1) / (x2 - x1)) * (x - x1) + y2;
     }
 
-    public double getHoodPosition(double distance) {
-        if (distance < HOOD[0]) { // If distance less than smallest recorded distance
-            double lowerX = HOOD[0];
-            double lowerY = DISTANCE[0];
+    public double getDoubleLookupTable(double input, double[] xarr, double[] yarr) {
+        if (input < xarr[0]) { // If distance less than smallest recorded distance
+            double lowerX = xarr[0];
+            double lowerY = yarr[0];
 
-            double upperX = HOOD[1];
-            double upperY = DISTANCE[1];
+            double upperX = xarr[1];
+            double upperY = yarr[1];
 
-            return line(lowerX, lowerY, upperX, upperY, distance);
-        } else if (distance > HOOD[HOOD.length - 1]) { // If distance greater than largest recorded value
-            double lowerX = HOOD[HOOD.length - 2];
-            double lowerY = DISTANCE[DISTANCE.length - 2];
+            return line(lowerX, lowerY, upperX, upperY, input);
+        } else if (input > xarr[xarr.length - 1]) { // If distance greater than largest recorded value
+            double lowerX = xarr[xarr.length - 2];
+            double lowerY = yarr[yarr.length - 2];
 
-            double upperX = HOOD[HOOD.length - 1];
-            double upperY = DISTANCE[DISTANCE.length - 1];
+            double upperX = xarr[xarr.length - 1];
+            double upperY = yarr[yarr.length - 1];
 
-            return line(lowerX, lowerY, upperX, upperY, distance);
+            return line(lowerX, lowerY, upperX, upperY, input);
         } else { // If distance somewhere in the middle
-            for (int i = 0; i < HOOD.length - 1; i++) {
-                double lowerX = HOOD[i];
-                double lowerY = DISTANCE[i];
+            for (int i = 0; i < xarr.length - 1; i++) {
+                double lowerX = xarr[i];
+                double lowerY = yarr[i];
 
-                double upperX = HOOD[i + 1];
-                double upperY = DISTANCE[i + 1];
+                double upperX = xarr[i + 1];
+                double upperY = yarr[i + 1];
 
-                if ((lowerX < distance) && (distance < upperX)) { // HOOD[i-1] < distance < HOOD[i+1]
-                    return line(lowerX, lowerY, upperX, upperY, distance);
+                if ((lowerX < input) && (input < upperX)) { // HOOD[i-1] < distance < HOOD[i+1]
+                    return line(lowerX, lowerY, upperX, upperY, input);
                 }
             }
         }
