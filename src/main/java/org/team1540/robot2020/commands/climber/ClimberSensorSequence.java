d3c9b5d918f8f939b8bb584f958abf0324c3f562
Micah Powch <powchm@catlin.edu>
2020-02-18 17:07:11 -0800

    Disengage ratchet at beginning of ClimberSensorSequence

--- src/main/java/org/team1540/robot2020/commands/climber/ClimberSensorSequence.java
+++ src/main/java/org/team1540/robot2020/commands/climber/ClimberSensorSequence.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import org.team1540.robot2020.utils.ChickenXboxController;
+import org.team1540.robot2020.utils.InstCommand;
 
 public class ClimberSensorSequence extends SequentialCommandGroup {
 
@@ -11,6 +12,7 @@ public class ClimberSensorSequence extends SequentialCommandGroup {
 
     public ClimberSensorSequence(Climber climber, ChickenXboxController.Axis axis) {
         addCommands(
+                new InstCommand(()->climber.setRatchet(Climber.RatchetState.DISENGAGED)),
                 new ClimberMoveToMeters(climber, () -> openHookMeters),
                 new ParallelCommandGroup(
                         new ClimberJoystickControl(climber, axis),
