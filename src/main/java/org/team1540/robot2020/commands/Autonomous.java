5103eb9c52135c450e9bb474d48688c69b2c84f3
Nathan Merrill <mathiscool3000@gmail.com>
2020-02-21 18:09:10 +0000

    In progress v2

--- src/main/java/org/team1540/robot2020/commands/Autonomous.java
+++ src/main/java/org/team1540/robot2020/commands/Autonomous.java
@@ -31,8 +31,8 @@ public class Autonomous extends ParallelCommandGroup {
                 new HoodZeroSequence(hood),
                 sequence(
                     new ChickenRamseteCommand(
-                            () -> new Pose2d(0, 0, new Rotation2d(0)),
-                            startingPose,
+                            () -> startingPose,
+                            new Pose2d(0, 0, new Rotation2d(0)),
                             new Pose2d(1.5, 0.5, new Rotation2d(0)),
                             false,
                             localizationManager,
