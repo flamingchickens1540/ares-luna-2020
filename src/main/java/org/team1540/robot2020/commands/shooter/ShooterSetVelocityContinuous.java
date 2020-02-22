fb304f0b1ead8c2252c8f2d8547adf2056866644
Liam Wang <5054950+wangl5@users.noreply.github.com>
2020-02-22 15:17:44 -0800

    Add continuous D scaling

--- src/main/java/org/team1540/robot2020/commands/shooter/ShooterSetVelocityContinuous.java
+++ src/main/java/org/team1540/robot2020/commands/shooter/ShooterSetVelocityContinuous.java
@@ -1,49 +1,47 @@
 package org.team1540.robot2020.commands.shooter;
 
 import edu.wpi.first.wpilibj.SlewRateLimiter;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import org.team1540.robot2020.utils.ControlUtils;
 
 import java.util.function.DoubleSupplier;
 
 public class ShooterSetVelocityContinuous extends CommandBase {
-    public static final int LOW_RPM_THRESH = 3000;
-    public static final int LOW_RPM_BUFFER = 200;
     public static final int HIGH_RPM_kD = 30;
     public static final int LOW_RPM_kD = 10;
     private DoubleSupplier targetRPMSupplier;
     private Shooter shooter;
+    private Timer kdTimer;
 
     private SlewRateLimiter velocityRateLimiter = new SlewRateLimiter(3000);
-    private boolean lowRpmMode;
 
     public ShooterSetVelocityContinuous(Shooter shooter, DoubleSupplier targetRPMSupplier) {
         this.shooter = shooter;
         this.targetRPMSupplier = targetRPMSupplier;
         addRequirements(shooter);
+        kdTimer = new Timer();
     }
 
     @Override
     public void initialize() {
         velocityRateLimiter.reset(shooter.getVelocityRPM());
-        lowRpmMode = false;
         shooter.config_kD(HIGH_RPM_kD);
+        kdTimer.reset();
+        kdTimer.start();
     }
 
     private void configLowRPM(double targetRPM) {
-        if (lowRpmMode && targetRPM > LOW_RPM_THRESH + LOW_RPM_BUFFER) {
-            lowRpmMode = false;
-            shooter.config_kD(HIGH_RPM_kD);
-        } else if (!lowRpmMode && targetRPM < LOW_RPM_THRESH - LOW_RPM_BUFFER) {
-            lowRpmMode = true;
-            shooter.config_kD(LOW_RPM_kD);
-        }
+        double kD = ControlUtils.linearDeadzoneRamp(targetRPM, false, HIGH_RPM_kD, LOW_RPM_kD, 5000, 3000);
+        shooter.config_kD(kD);
+        SmartDashboard.putNumber("ShooterLineUpSequence/calculatedKd", kD);
     }
 
     @Override
     public void execute() {
         double targetRPM = this.targetRPMSupplier.getAsDouble();
-        configLowRPM(targetRPM);
+        if (kdTimer.hasPeriodPassed(0.3)) configLowRPM(targetRPM);
         shooter.setVelocityRPM(velocityRateLimiter.calculate(targetRPM));
         SmartDashboard.putNumber("ShooterLineUpSequence/shooterSetpoint", targetRPM);
     }
