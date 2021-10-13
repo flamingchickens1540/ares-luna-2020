76ee6b5086f3125bee29d9dec8099599e08ceb91
Nate Sales <nate@natesales.net>
2021-10-12 21:12:12 -0700

    feat: only log drivetrain values from avian

--- src/main/java/org/team1540/robot2020/commands/drivetrain/AvianDrive.java
+++ src/main/java/org/team1540/robot2020/commands/drivetrain/AvianDrive.java
@@ -1,12 +1,14 @@
 package org.team1540.robot2020.commands.drivetrain;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import org.apache.log4j.Logger;
+import org.team1540.robot2020.RobotContainer;
 import org.team1540.robot2020.utils.Avian;
 
 public class AvianDrive extends CommandBase {
+    private static final Logger logger = Logger.getLogger(RobotContainer.class);
     private final DriveTrain driveTrain;
     private final Avian avian;
-
     private final double turnSpeed = 0.1;
     private final double driveSpeed = 0.1;
 
@@ -17,26 +19,31 @@ public class AvianDrive extends CommandBase {
         addRequirements(driveTrain);
     }
 
+    private void setDriveTrain(double leftPercent, double rightPercent) {
+        logger.info(leftPercent + ", " + rightPercent);
+        // driveTrain.setPercent(leftPercent, rightPercent);
+    }
+
     @Override
     public void execute() {
         // Stop if hands aren't detected
         if (avian.getDouble("avian/detected_hands") <= 2) {
-            driveTrain.setPercent(0, 0);
+            setDriveTrain(0, 0);
             return;
         }
 
         // Pinch to turn
         if (avian.getBooleanExclusive("avian/left_pinch")) {
-            driveTrain.setPercent(-turnSpeed, turnSpeed);
+            setDriveTrain(-turnSpeed, turnSpeed);
         } else if (avian.getBooleanExclusive("avian/right_pinch")) {
-            driveTrain.setPercent(turnSpeed, -turnSpeed);
+            setDriveTrain(turnSpeed, -turnSpeed);
         }
 
         // Fist to drive
         if (avian.getBooleanExclusive("avian/left_fist")) {
-            driveTrain.setPercent(driveSpeed, driveSpeed);
+            setDriveTrain(driveSpeed, driveSpeed);
         } else if (avian.getBooleanExclusive("avian/right_fist")) {
-            driveTrain.setPercent(-driveSpeed, -driveSpeed);
+            setDriveTrain(-driveSpeed, -driveSpeed);
         }
     }
 }
